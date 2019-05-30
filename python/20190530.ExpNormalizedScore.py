a62a36b9c2ad4196261f0713479a78214fedcb23
Jonathan Godbout <jgodbout@bathfitter.com>
2019-05-30 16:42:30 -0400

    Added a plot of a normalized score using exp

---
+++ python/20190530.ExpNormalizedScore.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.style.use(['dark_background'])
+plt.rcParams['grid.color'] = '#333333'
+plt.rcParams['figure.facecolor'] = '#222222'
+plt.rcParams['axes.facecolor'] = '#222222'
+
+plt.close('all')
+
+tolLabels = ['0.010', '0.020', '0.030']
+for tolLabel in tolLabels:
+    tol = float(tolLabel)
+    rms = np.arange(0.00001, 0.05, 0.0001)
+    score = np.exp(1 - rms/tol) / np.exp(1)
+    plt.plot(rms, score)
+
+plt.xlabel('rms')
+plt.ylabel('score')
+plt.title('effect of normalization distance')
+plt.legend(tolLabels)
+
+plt.show()
