0587946444f4b9cc3fac4ff0f4993fc7a118cef8
Jonathan Godbout <jgodbout@bathfitter.com>
2019-06-04 13:40:49 -0400

    Reverse computation to get rms from score

--- python/20190530.ExpNormalizedScore.py
+++ python/20190530.ExpNormalizedScore.py
@@ -10,6 +10,9 @@ plt.rcParams['axes.facecolor'] = '#222222'
 plt.close('all')
 
 tolLabels = ['0.010', '0.020', '0.030']
+
+plt.figure()
+
 for tolLabel in tolLabels:
     tol = float(tolLabel)
     rms = np.arange(0.00001, 0.05, 0.0001)
@@ -19,6 +22,19 @@ for tolLabel in tolLabels:
 plt.xlabel('rms')
 plt.ylabel('score')
 plt.title('effect of normalization distance')
+plt.legend(tolLabels)
+
+plt.figure()
+
+for tolLabel in tolLabels:
+    tol = float(tolLabel)
+    score = np.arange(0.05, 1.0, 0.001)
+    rms = -tol*np.log(score)
+    plt.plot(score, rms)
+
+plt.xlabel('score')
+plt.ylabel('rms')
+plt.title('reverse compute rms from score')
 plt.legend(tolLabels)
 
 plt.show()
