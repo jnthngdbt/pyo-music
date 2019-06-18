2384095b1f46f188eeb858ce3a3c234d43fa2d6a
Jonathan Godbout <jgodbout@bathfitter.com>
2019-06-18 15:27:37 -0400

    Computing planes stats, update scans data

--- python/20190617.PlanesAnalysis.py
+++ python/20190617.PlanesAnalysis.py
@@ -1,3 +1,5 @@
+#%%
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -8,29 +10,28 @@ matplotlib.style.use(['dark_background'])
 plt.rcParams['grid.color'] = '#333333'
 plt.rcParams['figure.facecolor'] = '#222222'
 plt.rcParams['axes.facecolor'] = '#222222'
-plt.rcParams["font.size"] = "10"
+plt.rcParams["font.size"] = "11"
 plt.rcParams["font.family"] = "consolas"
 
 plt.close('all')
 
-# -----------------------------------------------------------------------
-# Some feature subsets. Select one to use.
+#%% Some feature subsets. Select one to use.
 
-featureSubset_ABCD = [ 'mold', 'backA', 'backB', 'backC', 'backD', 'frontA', 'frontB', 'frontC', 'frontD', 'bottomA', 'bottomB', 'bottomC', 'bottomD', 'topA', 'topB', 'topC', 'topD', 'rightA', 'rightB', 'rightC', 'rightD', 'leftA', 'leftB', 'leftC', 'leftD']
-featureSubset_ABCDK = [ 'mold', 'backK', 'frontK', 'bottomK', 'topK', 'rightK', 'leftK', 'backA', 'backB', 'backC', 'backD', 'frontA', 'frontB', 'frontC', 'frontD', 'bottomA', 'bottomB', 'bottomC', 'bottomD', 'topA', 'topB', 'topC', 'topD', 'rightA', 'rightB', 'rightC', 'rightD', 'leftA', 'leftB', 'leftC', 'leftD']
+featureSubset_ABCD = [ 'id', 'mold', 'backA', 'backB', 'backC', 'backD', 'frontA', 'frontB', 'frontC', 'frontD', 'bottomA', 'bottomB', 'bottomC', 'bottomD', 'topA', 'topB', 'topC', 'topD', 'rightA', 'rightB', 'rightC', 'rightD', 'leftA', 'leftB', 'leftC', 'leftD']
+featureSubset_ABCDK = [ 'id', 'mold', 'backK', 'frontK', 'bottomK', 'topK', 'rightK', 'leftK', 'backA', 'backB', 'backC', 'backD', 'frontA', 'frontB', 'frontC', 'frontD', 'bottomA', 'bottomB', 'bottomC', 'bottomD', 'topA', 'topB', 'topC', 'topD', 'rightA', 'rightB', 'rightC', 'rightD', 'leftA', 'leftB', 'leftC', 'leftD']
 
 # Selection
 featureSubset = featureSubset_ABCDK
 
-# -----------------------------------------------------------------------
+#%%
 
 scanDataFull = pd.read_csv("python/data/20190617.planes.scans.csv", index_col=False)
 moldDataFull = pd.read_csv('python/data/20190617.planes.molds.csv', index_col=False)
 moldScanDataFull = pd.read_csv('python/data/20190617.planes.moldscans.csv', index_col=False)
 
-print(scanDataFull)
-print(moldDataFull)
-print(moldScanDataFull)
+# print(scanDataFull)
+# print(moldDataFull)
+# print(moldScanDataFull)
 
 def removeNanRows(df):
     nanRows = df.isna().any(axis=1)
@@ -59,28 +60,29 @@ moldScanData = moldScanDataFull.loc[:, featureSubset]
 
 # plt.show()
 
-# -----------------------------------------------------------------------
+#%%
 
 scanData['type'] = 'scan'
 moldData['type'] = 'mold'
 moldScanData['type'] = 'moldscan'
 
 print('added data type column')
-print(scanData)
-print(moldData)
-print(moldScanData)
+# print(scanData)
+# print(moldData)
+# print(moldScanData)
 
-data = moldData ############################## scandata
+data = scanData
+data = data.append(moldData, ignore_index=True)
 data = data.append(moldScanData, ignore_index=True)
 
 print('appended into a single dataframe')
-print(data)
+# print(data)
 
 # data.groupby('type').mean().plot.bar()
 
 # plt.show()
 
-# -----------------------------------------------------------------------
+#%%
 
 def getMoldRowIdx(moldIdx):
     i = data.index[(data['type'] == 'mold') & (data['mold'] == moldIdx)].values
@@ -95,7 +97,7 @@ data = removeNanRows(data)
 data['moldRow'] = [int(i) for i in data['moldRow']] # convert to int
 
 print('added moldRow')
-print(data)
+# print(data)
 
 print('testing moldRow')
 print('does {} == {}?'.format(
@@ -105,38 +107,127 @@ print('does {} == {}?'.format(
 # plt.plot(data.loc[:, 'moldRow'])
 # plt.show()
 
-# -----------------------------------------------------------------------
+#%%
 
-def getNormalFromIdx(str, i):
-    n = data.loc[i, [str+'A', str+'B', str+'C']].values
+planeNames = ['back', 'front', 'top', 'bottom', 'left', 'right']
+
+planeAngleDiffNames = [plane + '-angle-diff' for plane in planeNames]
+planeDistDiffNames = [plane + '-dist-diff' for plane in planeNames]
+planeAngleBaselineNames = [plane + '-angle-baseline' for plane in planeNames]
+planeDistBaselineNames = [plane + '-dist-baseline' for plane in planeNames]
+
+def getNormalFromIdx(plane, i):
+    n = data.loc[i, [plane+'A', plane+'B', plane+'C']].values
     n = n / np.sqrt(np.sum(n*n))
     return n
 
-def computeNormalsDot(str, i, j):
-    ni = getNormalFromIdx(str, i)
-    nj = getNormalFromIdx(str, j)
-    d = np.dot(ni, nj)
+def computeNormalsDot(plane, i, j):
+    ni = getNormalFromIdx(plane, i)
+    nj = getNormalFromIdx(plane, j)
+    d = min(1.0, np.dot(ni, nj))
     return d
 
-data['backDot'] = [computeNormalsDot('back', i, data.loc[i, 'moldRow']) for i in data.index]
+def convertDotToAngleDeg(dot):
+    a = 180.0 * np.arccos(dot) / np.pi
+    return a
 
-print('computed normals dot')
-print(data)
+def computeNormalsAngleDeg(plane, i, j):
+    return convertDotToAngleDeg(computeNormalsDot(plane, i, j))
 
-# -----------------------------------------------------------------------
+for idx, plane in enumerate(planeNames):
+    data[planeAngleDiffNames[idx]] = [computeNormalsAngleDeg(plane, i, data.loc[i, 'moldRow']) for i in data.index]
 
-# def computeControlNormalsDot(str, i):
+print('computed normals angles diff')
+print(data[planeAngleDiffNames].tail())
+
+#%%
+
+def getDistFromIdx(plane, i):
+    d = data.loc[i, plane+'D'] * 100 # convert to cm
+    return d
+
+def computeDistDiff(plane, i, j):
+    di = getDistFromIdx(plane, i)
+    dj = getDistFromIdx(plane, j)
+    d = di-dj
+    return d
+
+for idx, plane in enumerate(planeNames):
+    data[planeDistDiffNames[idx]] = [computeDistDiff(plane, i, data.loc[i, 'moldRow']) for i in data.index]
+
+print('computed d (distance) diff')
+print(data[planeDistDiffNames].tail())
+
+#%%
+
+# def computeAngleBaseline(plane, i):
+#     N = data.shape[0]
+#     a = 0
+#     nbRandomTrials = 10
+#     for i in np.arange(nbRandomTrials):
+#         ri = np.random.randint(N)
+#         a += convertDotToAngleDeg(computeNormalsDot(plane, int(i), data.index[ri]))
+#     return a / nbRandomTrials
+
+# for idx, plane in enumerate(planeNames):
+#     data[planeAngleBaselineNames[idx]] = [computeAngleBaseline(plane, i) for i in data.index]
+
+# print('computed angle baseline diff')
+# print(data[planeAngleBaselineNames].tail())
+
+#%%
+
+# def computeDistBaseline(plane, i):
 #     N = data.shape[0]
 #     d = 0
 #     nbRandomTrials = 10
 #     for i in np.arange(nbRandomTrials):
 #         ri = np.random.randint(N)
-#         d += computeNormalsDot(str, i, data.index[ri])
-    
+#         d += getDistFromIdx(plane, i) - getDistFromIdx(plane, ri)
 #     return d / nbRandomTrials
 
-# data['backControlDot'] = [computeControlNormalsDot('back', i) for i in data.index]
+# for idx, plane in enumerate(planeNames):
+#     data[planeDistBaselineNames[idx]] = [computeDistBaseline(plane, i) for i in data.index]
 
-# -----------------------------------------------------------------------
+# print('computed dist baseline diff')
+# print(data[planeDistBaselineNames].tail())
+
+#%%
+
+def selectMoldScans(data): return data['type'] == 'moldscan'
+def selectScans(data): return data['type'] == 'scan'
+
+def plot(colNames, bins, xlabel):
+    plt.figure(figsize=(8,8))
+    for i, plane in enumerate(planeNames):
+        moldScanDiff = data.loc[selectMoldScans(data), colNames[i]]
+        scanDiff = data.loc[selectScans(data), colNames[i]]
+
+        alpha = 0.5
+
+        plt.subplot(3,2,i+1)
+        plt.hist(moldScanDiff, bins=bins, alpha=alpha, label='mold')
+        plt.hist(scanDiff, bins=bins, alpha=alpha, label='scan')
+        plt.title(plane)
+
+        if i > 3:
+            plt.xlabel(xlabel)
+
+    plt.legend(['mold', 'scan'])
+
+plot(planeAngleDiffNames, np.arange(0, 10, 0.2), 'degree')
+plt.suptitle('angle diff')
+plt.tight_layout()
+
+plot(planeDistDiffNames, np.arange(-10, 10, 0.4), 'cm')
+plt.suptitle('distance diff')
+plt.tight_layout()
+
+#%%
+
+# Cases of the second mode of top/bottom D distribution
+weird = data.loc[data['top-dist-diff'] > 5, ['id', 'mold']]
+print(weird)
+weird.hist()
 
 plt.show()
