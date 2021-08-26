9af76e633759304d5e800dae7eda253de79b0831
Jonathan Godbout <jgodbout@bathfitter.com>
2021-08-26 15:18:32 -0400

    Added spectrogram based implementation

--- python/20210823.sample.ambient.generator.2.song.py
+++ python/20210823.sample.ambient.generator.2.song.py
@@ -73,7 +73,7 @@ def processSample(x, k):
   F = randomizePhase(F)
 
   x = np.real(np.fft.ifft(F, axis=0))
-  x = applyWindow(x, signal.windows.hann(int(0.9 * x.shape[0])))
+  x = applyWindow(x, signal.windows.hann(int(x.shape[0])))
 
   return x
 
@@ -96,11 +96,12 @@ def mixSignal(s, x, n):
     return y
 
 ## -------------------------------------------------------
+# seg = audiosegment.from_file("./data/03 Mission Two.m4a")
+seg = audiosegment.from_file("./data/04 Mission Three.m4a")
+# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
+# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
+# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
 # seg = audiosegment.from_file("./data/11 Mission Ten.m4a")
-# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
-# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
-# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
-# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
 # seg = audiosegment.from_file("./data/Big Rock.1.m4a")
 # seg = audiosegment.from_file("./data/Alone.3.m4a")
 # seg = audiosegment.from_file("./data/Jump.12.m4a")
@@ -110,7 +111,7 @@ def mixSignal(s, x, n):
 # seg = audiosegment.from_file("./data/Aly Wood 2.m4a")
 # seg = audiosegment.from_file("./data/Beverly Aly Hills 5.m4a")
 # seg = audiosegment.from_file("./data/insects.m4a")
-seg = audiosegment.from_file("./data/smallthings.m4a")
+# seg = audiosegment.from_file("./data/smallthings.m4a")
 
 fs = seg.frame_rate
 x = seg.to_numpy_array()
@@ -121,16 +122,24 @@ if x.ndim == 1:
   x = np.concatenate([x, x], axis=1)
 
 ## -------------------------------------------------------
-Ns = 44000
-Nw = 11000
-Tk = 10 # desired final sample duration, should be longer than sample duration
+Ts = 4. # step duration
+Tw = 0.25 # sample duration
+Tk = 8 # desired final sample duration, should be longer than sample duration
 
-Nk = Tk * fs
+Ns = int(Ts * fs)
+Nw = int(Tw * fs)
+Nk = int(Tk * fs)
 k = int(Nk / Nw)
 
+nbSteps = x.shape[0] - Nw - 1
+
 s = []
-for i in np.arange(x.shape[0] - Nw - 1, step=Ns):
-  s = mixSignal(s, processSample(x[i:i+Nw, :], k), int(0.5 * Nk))
+for i in np.arange(nbSteps, step=Ns):
+  print('{0}/{1}'.format(i, nbSteps))
+  s = mixSignal(s, processSample(x[i:i+Nw, :], k), int(0.1 * Nk))
+
+sos = signal.butter(2, 3000, 'lp', fs=fs, output='sos')
+s = signal.sosfilt(sos, s, axis=0)
 
 y = audiosegment.from_numpy_array(discretize(s), fs)
 y.export("./songs/sample.ambient.generated.song.m4a")
---
+++ python/20210823.sample.ambient.generator.3.spectrogram.py
@@ -0,0 +1,192 @@
+# 20210823.sample.ambient.generator.py
+
+import audiosegment
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import argmax
+import scipy.io.wavfile
+import winsound
+import os
+from scipy import signal
+from threading import Thread
+
+matplotlib.style.use(['dark_background'])
+plt.rcParams['grid.color'] = '#333333'
+plt.rcParams['figure.facecolor'] = '#222222'
+plt.rcParams['axes.facecolor'] = '#222222'
+plt.rcParams["font.size"] = "11"
+plt.rcParams["font.family"] = "consolas"
+
+plt.close('all')
+
+## -------------------------------------------------------
+def discretize(x, volume=1.0):
+  A = volume * (2**15-1) # int16 scale factor; 2^16/2, since signed, -1 to avoid saturation
+  x *= A/np.max(np.abs(x)) # normalize the signal to span the int16 domain
+  return x.astype(np.int16)
+
+def write(x, name, volume=1.0):
+  scipy.io.wavfile.write(name, fs, discretize(x, volume))
+
+def loop(s, f):
+  play(s, f, loop=True)
+
+def play(s, f, volume=1.0, loop=False):
+  f = "./songs/" + f
+  write(s, f, volume=volume)
+  play = True
+  while (play):
+    winsound.PlaySound(f, 0)
+    play = loop
+
+## -------------------------------------------------------
+def interpolateFft(F, k):
+  Nf = F.shape[0]
+  Ng = int(Nf * k)
+
+  Gr = np.zeros((Ng, 2))
+  Gi = np.zeros((Ng, 2))
+  G = np.zeros((Ng, 2))
+
+  f = np.fft.fftfreq(Nf)
+  f = np.fft.fftshift(f)
+  F = np.fft.fftshift(F, axes=0)
+
+  g = np.linspace(f[0], f[-1], num=Ng)
+
+  for i in np.arange(F.shape[1]):
+    Gr[:,i] = np.interp(g, f, F[:,i].real)
+    Gi[:,i] = np.interp(g, f, F[:,i].imag)
+
+  G = Gr + 1j * Gi
+  G = np.fft.fftshift(G)
+
+  return G
+
+def randomizePhase(F):
+  F = np.abs(F) * np.exp(1j * np.random.rand(F.shape[0], F.shape[1]) * np.pi)
+  return F
+
+def applyWindow(x, w):
+  Nw = int(0.5 * len(w))
+  Nx = x.shape[0]
+
+  for i in np.arange(x.shape[1]):
+    x[0:Nw, i] = x[0:Nw, i] * w[0:Nw]
+    x[Nx-Nw:, i] = x[Nx-Nw:, i] * w[Nw:]
+
+  return x
+
+## -------------------------------------------------------
+def spectrogram(x, fs, Ts, Tw):
+  stepLen = int(Ts * fs)
+  windowLen = int(Tw * fs)
+
+  maxPos = x.shape[0] - windowLen - 1
+  nbSteps = int(maxPos / stepLen) - 1
+  nbChans = x.shape[1]
+
+  S = np.zeros((windowLen, nbSteps, nbChans))
+  
+  for i in np.arange(nbSteps):
+    print('{0}/{1}'.format(i, nbSteps))
+    pos = i * stepLen
+    xi = x[pos: pos + windowLen, :]
+    S[:, i, :] = np.fft.fft(xi, axis=0)
+
+  return S
+
+def reconstructSample(F, k, w):
+  F = interpolateFft(F, k)
+  F = randomizePhase(F)
+
+  x = np.real(np.fft.ifft(F, axis=0))
+  x = applyWindow(x, signal.windows.hann(int(w * x.shape[0])))
+
+  return x
+
+def filterSound(x, fc, fs):
+  sos = signal.butter(6, fc, 'lp', fs=fs, output='sos')
+  x = signal.sosfilt(sos, x, axis=0)
+  return x
+
+def boostBass(x, k, fc, fs):
+  sos = signal.butter(2, fc, 'lp', fs=fs, output='sos')
+  return x + k * signal.sosfilt(sos, x, axis=0)
+
+def exportCompressed(x, name, fs):
+  y = audiosegment.from_numpy_array(discretize(x), fs)
+  y.export(name)
+
+def playSound(x):
+  tmpWav = "temp.wav"
+  loop(x, tmpWav)
+
+## -------------------------------------------------------
+# seg = audiosegment.from_file("./data/03 Mission Two.m4a")
+# seg = audiosegment.from_file("./data/04 Mission Three.m4a")
+# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
+# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
+# seg = audiosegment.from_file("./data/07 Mission Six.m4a")
+# seg = audiosegment.from_file("./data/11 Mission Ten.m4a")
+# seg = audiosegment.from_file("./data/Big Rock.1.m4a")
+# seg = audiosegment.from_file("./data/Alone.3.m4a")
+# seg = audiosegment.from_file("./data/Jump.12.m4a")
+# seg = audiosegment.from_file("./data/Press.5.m4a")
+# seg = audiosegment.from_file("./data/Late.06.m4a")
+# seg = audiosegment.from_file("./data/Sam Sung 3.m4a") # wow
+# seg = audiosegment.from_file("./data/Aly Wood 2.m4a")
+seg = audiosegment.from_file("./data/Beverly Aly Hills 5.m4a") # t: 3.55, 7.5, 12.6
+# seg = audiosegment.from_file("./data/insects.m4a")
+# seg = audiosegment.from_file("./data/smallthings.m4a") # t: 31.55, 47.95
+
+fs = seg.frame_rate
+x = seg.to_numpy_array()
+x = x / x.max()
+
+if x.ndim == 1:
+  x = x.reshape(len(x), 1)
+  x = np.concatenate([x, x], axis=1)
+
+## -------------------------------------------------------
+Ts = 0.05 # step duration
+Tw = 0.25 # sample duration
+lowPass = 10000
+doBoostBass = False # when using a recording
+
+x = filterSound(x, lowPass, fs)
+if doBoostBass:
+  x = boostBass(x, 5, 500, fs)
+
+exportCompressed(x, "./songs/sample.ambient.generated.song.m4a", fs)
+
+S = spectrogram(x, fs, Ts, Tw)
+P = np.log(np.abs(S))
+
+t = np.linspace(0, x.shape[0] / fs, num=S.shape[1])
+f = np.fft.fftfreq(S.shape[0], 1 / fs)
+
+## -------------------------------------------------------
+Tk = 20 # desired final sample duration
+maxFreq = 6000
+timePosSec = 3.55
+
+ti = argmax(t > timePosSec) # sample index to play
+
+Si = np.squeeze(S[:,ti,:])
+s = reconstructSample(Si, Tk / Tw, 0.5)
+
+exportCompressed(s, "./songs/sample.ambient.generated.sample.m4a", fs)
+
+music_thread = Thread(target=lambda: playSound(s))
+music_thread.start()
+
+## ------------------------------------------------------
+
+fmax = argmax(f > maxFreq)
+P0 = P[:fmax, :, 0]
+f0 = f[:fmax]
+plt.pcolormesh(t, f0, P0)
+
+plt.show()
