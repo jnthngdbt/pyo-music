cfc35adc1d5f43ed42336cd116696b133ebaca50
Jonathan Godbout <jgodbout@bathfitter.com>
2022-01-07 21:41:37 -0500

    Changed notes pool

--- python/20220107.pyo.tiagovaz.pattern.py
+++ python/20220107.pyo.tiagovaz.pattern.py
@@ -9,6 +9,14 @@ def pat():
     f = random.randrange(200, 401, 25)
     a.freq = [f, f+1]
 
-p = Pattern(pat, .125)
+def scale():
+    # pentatonic scales
+    major = [0,2,4,7,9,12]
+    minor = [0,3,5,7,10,12]
+    m = random.choice(major)
+    f = midiToHz(m + 48)
+    a.freq = [f, f+1]
+
+p = Pattern(scale, .125)
 p.play()
 s.gui(locals())
