52ee235b87de71ddb029db83a69bc6289cbe5473
Jonathan Godbout <jgodbout@bathfitter.com>
2022-01-10 13:50:43 -0500

    Refactored using arrays

--- python/20220107.pyo.tone.harm.chord.drone.pan.py
+++ python/20220107.pyo.tone.harm.chord.drone.pan.py
@@ -2,52 +2,50 @@ from pyo import *
 import numpy as np
 
 s = Server().boot()
+s.start()
 
 h = HarmTable([x**-1.8 for x in np.arange(1, 10)])
 # h = HarmTable([.1,0,.2,0,.1,0,0,0,.04,0,0,0,0.02])
 # h = HarmTable([.5, .7, .7, .5, .3, .1, .05, .02])
 
 root = 40
-lfo = 0.05
-ampLevel = 0.1
-ampScale = 0.5 * ampLevel
+notes = [0, 4, 7, 9, 12]
+nbNotes = len(notes)
 
-fa = midiToHz(0 + root )
-fb = midiToHz(7 + root )
-fc = midiToHz(12 + root)
-fd = midiToHz(16 + root)
+def randRange(a,b):
+  r = b - a
+  return a + r * np.random.rand()
 
-lfa = Sine(freq=lfo + 0.00, phase=0.2)
-lfb = Sine(freq=lfo - 0.02, phase=0.4)
-lfc = Sine(freq=lfo + 0.02, phase=0.6)
-lfd = Sine(freq=lfo - 0.02, phase=0.8)
+def randPhase():
+  return 2.0 * np.pi * np.random.rand()
 
-a = Osc(h, freq=[fa, fa + 1], mul=1.0 * ampLevel + ampScale * lfa).mix(2)
-b = Osc(h, freq=[fb, fb + 1], mul=1.0 * ampLevel + ampScale * lfb).mix(2)
-c = Osc(h, freq=[fc, fc + 1], mul=0.8 * ampLevel + ampScale * lfc).mix(2)
-d = Osc(h, freq=[fd, fd + 1], mul=0.5 * ampLevel + ampScale * lfd).mix(2)
+oscLfos = [None] * nbNotes
+oscs = [None] * nbNotes
 
-# Using amplitude LFOs for panning, but mixing sources.
-pa = Pan(a, outs=2, pan=0.5 + 0.2 * lfd)
-pb = Pan(b, outs=2, pan=0.5 + 0.2 * lfc)
-pc = Pan(c, outs=2, pan=0.5 + 0.2 * lfb)
-pd = Pan(d, outs=2, pan=0.5 + 0.2 * lfa)
+panLfos = [None] * nbNotes
+pans = [None] * nbNotes
 
-x = pa + pb + pc + pd
-# x = a + b + c + d
-# x.out()
+for i, note in enumerate(notes):
+  f = midiToHz(note + root)
 
-# lfp = Sine(freq=1, mul=.5, add=.5)
-# p = Pan(x, outs=2, pan=lfp).out()
+  oscLfos[i] = LFO(randRange(0.01, 0.04), sharp=0.5, type=3).range(0, 1)
+  oscs[i] = Osc(h, freq=[f, f + 1], mul=oscLfos[i]).mix(2)
+
+  panLfos[i] = LFO(randRange(0.01, 0.04), sharp=0.5, type=3).range(0.3, 0.7)
+  pans[i] = Pan(oscs[i], outs=2, pan=panLfos[i])
+
+x = 0.0 * Sine()
+for sig in pans:
+  x = x + sig
 
 d = Delay(x, delay=[.15,.2,.25, .3, .35], feedback=.5, mul=.4)
-d.out()
+# d.out()
 d.ctrl()
 
-r = Freeverb(d, size=[.79,.8], damp=.9, bal=.3)
+r = Freeverb(d, size=[.79,.8], damp=.9, bal=.3, mul=0.3)
 r.out()
 r.ctrl()
 
-# y = WGVerb(x).out()
+# NOTE: Scope does not work with LFO: wx._core.wxAssertionError: C++ assertion "x > double((-2147483647 - 1))...
 
 s.gui(locals())
