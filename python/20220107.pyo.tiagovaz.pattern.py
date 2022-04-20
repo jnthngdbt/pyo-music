c2ba0902a42f7fb38ab60e97955309b1ae691d86
Jonathan Godbout <jgodbout@bathfitter.com>
2022-04-20 11:00:43 -0400

    Commented unused method

--- python/20220107.pyo.tiagovaz.pattern.py
+++ python/20220107.pyo.tiagovaz.pattern.py
@@ -5,15 +5,15 @@ s = Server().boot()
 t = HarmTable([1,0,.33,0,.2,0,.143,0,.111])
 a = Osc(table=t, freq=[250,251], mul=.2).out()
 
-def pat():
-    f = random.randrange(200, 401, 25)
-    a.freq = [f, f+1]
+# def pat():
+#     f = random.randrange(200, 401, 25)
+#     a.freq = [f, f+1]
 
 def scale():
     # pentatonic scales
     major = [0,2,4,7,9,12]
     minor = [0,3,5,7,10,12]
-    m = random.choice(major)
+    m = random.choice(minor)
     f = midiToHz(m + 48)
     a.freq = [f, f+1]
 
