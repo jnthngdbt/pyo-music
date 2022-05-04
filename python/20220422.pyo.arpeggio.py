7c5c6dab5248aba7c5b54b4fdd5bf83874ee9a26
Jonathan Godbout <jgodbout@bathfitter.com>
2022-05-04 16:30:32 -0400

    Arpeggio cleanup

--- python/20220422.pyo.arpeggio.py
+++ python/20220422.pyo.arpeggio.py
@@ -3,12 +3,6 @@ import numpy as np
 
 s = Server().boot()
 
-notes = [0,2,4,5,7,9,11] # major
-notes = [0,2,4,7,9] # major
-notes = [0,7,10] # minor
-notes = [0,3,5,7,10] # minor
-notes = [0,7,11] # major
-
 def expand(notes=[48, 52, 55], octaves=[0,1,2]):
   x = []
   notes = np.array(notes)
@@ -16,13 +10,19 @@ def expand(notes=[48, 52, 55], octaves=[0,1,2]):
     x = np.concatenate([x, i * 12 + notes], axis=0)
   return x.tolist()
 
+notes = [0,2,4,5,7,9,11] # major
+notes = [0,2,4,7,9] # major
+notes = [0,7,10] # minor
+notes = [0,3,5,7,10] # minor
+notes = [0,4,7,11] # major
+
 class Arpeggio:
-  def __init__(self, root=49, notes=[0,3,5,7,10], bps=4.0, decay=0.25, mirror=False, mul=1.0) -> None:
+  def __init__(self, root=49, notes=[0,3,5,7,10], time=0.2, dur=0.2, sustain=0.75, doMirror=False, mul=1.0) -> None:
     self.idx = 0
     self.root = root
     self.notes = notes
 
-    if mirror:
+    if doMirror:
       self.notes = self.notes + self.notes[-2:0:-1] # weird slicing: all except first and last, reversed
 
     self.table = HarmTable([1]) # , 0.3, 0.1, 0.02, 0.005
@@ -31,13 +31,14 @@ class Arpeggio:
     self.effect = Freeverb(self.osc, size=0.8, damp=0.5, bal=0.5)
     self.effect.ctrl()
 
-    self.env = Adsr(attack=.012, decay=decay, sustain=.0, release=.0, dur=.2)
+    p = 0.012
+    self.env = Adsr(attack=p, decay=p, sustain=sustain, release=p, dur=dur)
     self.env.ctrl()
 
     self.out = mul * self.effect * self.env
     self.out.out()
 
-    self.pat = Pattern(self.play, 1./bps)
+    self.pat = Pattern(self.play, time)
     self.pat.play()
 
   def play(self):
@@ -47,7 +48,8 @@ class Arpeggio:
     self.env.play()
     self.idx = (self.idx + 1) % len(self.notes)
 
-a = Arpeggio(root=49, notes=expand(notes, octaves=[0,1,2]), bps=10, decay=0.2, mirror=True, mul=0.1)
+t = 0.125
+a = Arpeggio(root=73, notes=expand(notes, octaves=[0,1,2,3]), time=t, dur=t, sustain=.75, doMirror=False, mul=0.04)
 
 s.start()
 s.gui(locals())
