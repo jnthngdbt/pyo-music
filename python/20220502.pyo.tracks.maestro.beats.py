09d718c03da6b375d2aee1da050ea36eba04fa7f
Jonathan Godbout <jgodbout@bathfitter.com>
2022-05-10 16:13:18 -0400

    Still fine tuning

--- python/20220502.pyo.tracks.maestro.beats.py
+++ python/20220502.pyo.tracks.maestro.beats.py
@@ -5,34 +5,34 @@ from pyoband import band
 s = Server().boot()
 
 root = 28
-dur = .175
+dur = .15
 
 M = band.Maestro(time=dur, nbSectionsToggle=2, tracks=[
-        band.BassBeat(name="Bass Beat", mul = 0.2, root = root, adsr=[.02, .02, .8, .04], tone=[.4, 1, .6, .2, .1],
+        band.BassBeat(name="Bass Beat", mul = 0.08, root = root, adsr=[.02, .02, .8, .04], tone=[.4, 1, .6, .2, .1],
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ],
             note = [ 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]
         ),
-        band.BassBeat(name="High Beep", mul = 0.01, root = root+36, adsr=[.02, .02, .5, .04], tone=[1],
+        band.BassBeat(name="High Beep", mul = 0.012, root = root+36, adsr=[.02, .02, .5, .04], tone=[1],
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0 ],
             note = [ 12 ]
         ),
-        band.BassBeat(name="Low Mid Beat", mul = 0.038, root = root+12, adsr=[.02, .02, .5, .04], tone=[1, .8 , .3 ],
+        band.BassBeat(name="Low Mid Beat", mul = 0.03, root = root+12, adsr=[.02, .02, .5, .04], tone=[1, .8, .1 ],
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ],
             note = [ 0, 0,16, 0, 0,16, 0, 0,16, 0, 0,16, 0, 0,16, 0, 0, 0,17, 0, 0,17, 0, 0,17, 0, 0,17, 0, 0,17, 0, 0, 0,16, 0, 0,16, 0, 0,16, 0, 0,16, 0, 0,16, 0, 0, 0,14, 0, 0,14, 0, 0,14, 0, 0,14, 0, 0,14, 0 ]
         ),
-        band.BassBeat(name="Mid Beat", mul = 0.02, root = root+36, adsr=[.02, .02, .5, .04], tone=[1 ],
+        band.BassBeat(name="Mid Beat", mul = 0.018, root = root+36, adsr=[.02, .02, .5, .04], tone=[1 ],
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ],
             note = [ 0, 0,12, 0, 0,12, 0, 0,12, 0, 0,12, 0, 0,12, 0, 0, 0, 9, 0, 0, 9, 0, 0, 9, 0, 0, 9, 0, 0, 9, 0, 0, 0, 7, 0, 0, 7, 0, 0, 7, 0, 0, 7, 0, 0, 7, 0, 0, 0,11, 0, 0,11, 0, 0,11, 0, 0,11, 0, 0,11, 0 ]
         ),
-        band.Kick(name="Kick", mul = 0.4, dur = dur,
+        band.Kick(name="Kick", mul = 0.2, dur = dur,
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0 ]
         ),
-        band.Snare(name="Snare", mul = 0.4, dur = dur, cutoff = 1000,
+        band.Snare(name="Snare", mul = 0.3, dur = dur, cutoff = 1000,
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 0, 0, 0, 0, 1, 0, 0, 0 ]
         ),
@@ -40,14 +40,14 @@ M = band.Maestro(time=dur, nbSectionsToggle=2, tracks=[
         #     #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
         #     beat = [ 1 ]
         # ),
-        # band.Arpeggio(name="Arpeggio 2", mul=0.005, root=root+48, notes=band.expand([0,4,5,7], octaves=[0,1]), dur=dur, sustain=.75, doMirror=True,
-        #     #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
-        #     beat = [ 1 ]
-        # ),
-        band.Arpeggio(name="Arpeggio 3", mul=0.006, root=root+36, notes=band.expand([0,4,7,0], octaves=[0,1,2,3]), dur=dur, sustain=.75, doMirror=False,
+        band.Arpeggio(name="Arpeggio 2", mul=0.006, root=root+36, notes=band.expand([0,4,5,7], octaves=[0,1,2]), dur=dur, sustain=.75, doMirror=True,
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1 ]
         ),
+        # band.Arpeggio(name="Arpeggio 3", mul=0.006, root=root+36, notes=band.expand([0,4,7,0], octaves=[0,1,2,3]), dur=dur, sustain=.75, doMirror=False,
+        #     #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
+        #     beat = [ 1 ]
+        # ),
 ])
 
 s.start()
--- python/20220503.pyo.punk.py
+++ python/20220503.pyo.punk.py
@@ -4,16 +4,16 @@ from pyoband import band
 
 s = Server().boot()
 
-time = 0.175
+time = 0.2
 root = 30
 
 M = band.Maestro(time=time, nbSectionsToggle=1, tracks=[
-        band.BassBeat(name="Bass Beat", mul = 0.04, root = root, dur=time, adsr=[.02, .02, .8, .04], tone=[.4, 1, .6, .2, .1],
+        band.BassBeat(name="Bass Beat", mul = 0.02, root = root, dur=time, adsr=[.02, .02, .8, .04], tone=[.4, 1, .6, .2, .1],
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1 ],
             note = [ 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]
         ),
-        band.BassBeat(name="Bass Harm", mul = 0.012, root = root+24, dur=time, adsr=[.01, .02, .5, .04], tone=[1., .8, .6],
+        band.BassBeat(name="Bass Harm", mul = 0.008, root = root+24, dur=time, adsr=[.01, .02, .5, .04], tone=[1., .8, .6],
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0, 1, 0, 1, 1, 1, 0 ],
             note = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
@@ -28,11 +28,11 @@ M = band.Maestro(time=time, nbSectionsToggle=1, tracks=[
             beat = [ 1, 0 ],
             note = [12,12,12, 7, 7, 7,12,12,12, 7, 7, 7,12,12, 7, 7]
         ),
-        band.Kick(name="Kick", mul = 0.25, dur = 0.15,
+        band.Kick(name="Kick", mul = 0.15, dur = 0.15,
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0, 0, 0, 1, 1, 0, 0  ]
         ),
-        band.SnarePunchy(name="Snare", mul = 0.25, dur = time, cutoff=800,
+        band.SnarePunchy(name="Snare", mul = 0.2, dur = time, cutoff=800,
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 0, 0, 1, 0 ]
         ),
