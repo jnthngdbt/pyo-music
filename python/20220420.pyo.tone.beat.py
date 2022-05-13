7865eb989ca045d2defdf75ca5f067f048c6530e
Jonathan Godbout <jgodbout@bathfitter.com>
2022-05-13 17:06:06 -0400

    Improved parameters, added reverb

--- python/20220420.pyo.tone.beat.py
+++ python/20220420.pyo.tone.beat.py
@@ -3,36 +3,31 @@ from pyo import *
 s = Server().boot()
 
 class ToneBeatSimple:
-  # 'palm mute': bps=8, decay=0.1
-  # 'smooth': bps=4, decay=0.25
-  def __init__(self, note=49, bps=4.0, decay=0.25, mul=1.0) -> None:
+  def __init__(self, note, dur, decay=0.25, reverb=.3, mul=1.0) -> None:
     f = midiToHz(note)
 
     self.table = HarmTable([1, 0.3, 0.1, 0.02, 0.005])
     self.note = Osc(table=self.table, freq=[f, f*1.01])
 
-    self.env = Adsr(attack=.01, decay=decay, sustain=.0, release=.0, dur=.2)
+    self.env = Adsr(attack=.01, decay=.9*dur, sustain=.0, release=.0, dur=dur)
     self.env.ctrl()
 
-    self.out = mul * self.note * self.env
+    self.reverb = Freeverb(self.note, size=[.79,.8], damp=.9, bal=reverb)
+    self.reverb.ctrl()
+
+    self.out = mul * self.reverb * self.env
     self.out.out()
 
-    self.pat = Pattern(self.play, 1./bps)
+    self.pat = Pattern(self.play, dur)
     self.pat.play()
 
   def play(self):
     self.env.play()
 
-class Peak:
-  def __init__(self, note=48, mul=1.0) -> None:
-    self.noise1 = PinkNoise()
-    self.noise2 = PinkNoise()
-    self.band = Biquadx([self.noise1, self.noise2], freq=midiToHz(note), q=12, type=2, stages=2, mul=mul).out()
-    self.band.ctrl()
+root = 27
+dur = 0.25
 
-pk = Peak(note=85, mul=1.0)
-tbSlow = ToneBeatSimple(note=49, bps=3.2, decay=0.24, mul=0.1)
-# tbFast = ToneBeatSimple(note=61, bps=8, decay=0.1, mul=0.00)
+tbSlow = ToneBeatSimple(note=root+24, dur=dur, decay=0.24, mul=0.1, reverb=.3)
 
 s.start()
 s.gui(locals())
