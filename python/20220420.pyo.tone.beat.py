a05f2e50beadfd7b66a83b2f75d8d57002593760
Jonathan Godbout <jon.godbout@gmail.com>
2022-07-27 09:08:52 -0400

    Adjustments

--- python/20220420.pyo.tone.beat.py
+++ python/20220420.pyo.tone.beat.py
@@ -3,13 +3,13 @@ from pyo import *
 s = Server().boot()
 
 class ToneBeatSimple:
-  def __init__(self, note, dur, decay=0.25, reverb=.3, mul=1.0) -> None:
+  def __init__(self, note, dur, decay=.1, sustain=.2, reverb=.3, mul=1.0) -> None:
     f = midiToHz(note)
 
     self.table = HarmTable([1, 0.3, 0.1, 0.02, 0.005])
     self.note = Osc(table=self.table, freq=[f, f*1.01])
 
-    self.env = Adsr(attack=.01, decay=.9*dur, sustain=.0, release=.0, dur=dur)
+    self.env = Adsr(attack=.01, decay=decay, sustain=sustain, release=.01, dur=dur)
     self.env.ctrl()
 
     self.reverb = Freeverb(self.note, size=[.79,.8], damp=.9, bal=reverb)
@@ -27,7 +27,7 @@ class ToneBeatSimple:
 root = 27
 dur = 0.25
 
-tbSlow = ToneBeatSimple(note=root+24, dur=dur, decay=0.24, mul=0.1, reverb=.3)
+tbSlow = ToneBeatSimple(note=root+24, dur=dur, decay=.1, sustain=.2, mul=0.1, reverb=.3)
 
 s.start()
 s.gui(locals())
