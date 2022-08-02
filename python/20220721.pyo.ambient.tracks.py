dc92d7f167e6d1f385e02d3cded7ff2def9e070c
Jonathan Godbout <jon.godbout@gmail.com>
2022-08-01 23:24:17 -0400

    AmbientE.Beat.High.1 mix

--- python/20220721.pyo.ambient.tracks.py
+++ python/20220721.pyo.ambient.tracks.py
@@ -1,3 +1,4 @@
+from attr import has
 from pyo import *
 import numpy as np
 
@@ -9,15 +10,12 @@ def randRange(a,b):
     return a + r * np.random.rand()
 
 class Base:
-    def __init__(self, lfo=0.015, cutoff=20000, mul=1.):
+    def __init__(self, lfo=0.015, cutoff=20000, phase=Sig(.75), mul=1.):
         self.lfo = lfo
         self.cutoff = cutoff
-
         self.amp = Sig(mul)
-        self.amp.ctrl()
-
-        self.lfoMul = self.amp * Sine(freq=self.lfo * randRange(0.6, 1.5), phase=0.75).range(0, 1)
-
+        self.phase = phase
+        self.lfoMul = self.amp * Sine(freq=self.lfo * randRange(0.6, 1.5), phase=self.phase).range(0, 1)
 
 class Pad (Base):
     def __init__(self, note, bw, damp, reverb=.3, **kwargs):
@@ -102,7 +100,6 @@ class ToneBeatSimple (Base):
                 durValue = dur.value
 
         self.env = Adsr(attack=.01, decay=decay, sustain=sustain, release=.01, dur=durValue)
-        # self.env.ctrl()
 
         self.lfoPan = Sine(freq=self.lfo * randRange(1.0, 2.0), phase=np.random.rand()).range(0.3, 0.7)
 
@@ -122,54 +119,96 @@ class ToneBeatSimple (Base):
     def play(self):
         self.env.play()
 
+# root = 27
+# dampFactor = 1.0
+# cutoff = 10000
+# volumeA = 1.0
+# volumeB = 1.0
+# phase = Sig(.75)
+# dt = Sig(.25)
+
+# phase.ctrl([SLMap(0., 1., 'lin', "value", phase.value)], "Phase") # NOTE: the 'name' must be the name of attribute
+# dt.ctrl([SLMap(0.1, 0.5, 'lin', "value", dt.value)], "Tempo") # NOTE: the 'name' must be the name of attribute
+
+# # Full drone
+# oscs = [
+#     Pad(note=root     , bw=40, damp=dampFactor*0.9, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.800), #  0.546   0.000
+#     Pad(note=root+12  , bw=40, damp=dampFactor*0.8, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.800), #  0.542   0.000
+#     Pad(note=root+24  , bw=40, damp=dampFactor*0.8, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.326), #  0.204   0.000
+#     Pad(note=root+24+7, bw=40, damp=dampFactor*0.8, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.123), #  0.077   0.000
+#     Pad(note=root+36  , bw=70, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.179), #  0.112   0.065
+#     Pad(note=root+36+7, bw=70, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.056), #  0.035   0.023
+#     Pad(note=root+48  , bw=70, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.080), #  0.050   0.073
+#     Pad(note=root+48+7, bw=70, damp=dampFactor*0.5, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.030), #  0.019   0.042
+#     Pad(note=root+60  , bw=70, damp=dampFactor*0.4, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.067), #  0.042   0.027
+#     Pad(note=root+60+7, bw=70, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.019), #  0.012   0.023
+#     Pad(note=root+72  , bw=70, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.040), #  0.042   0.027
+#     Pad(note=root+72+7, bw=70, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.010), #  0.012   0.023
+
+#     ToneBeatSimple(note=root   , dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.040, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
+#     ToneBeatSimple(note=root+12, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.040, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
+#     ToneBeatSimple(note=root+24, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.025, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
+#     ToneBeatSimple(note=root+36, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.012, tone=[1., .4, .2, .1, .05]), #  0.069   0.000
+#     ToneBeatSimple(note=root+48, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.008, tone=[1., .4, .2, .1, .05]), #  0.069   0.000
+# ]
+
 root = 27
 dampFactor = 1.0
 cutoff = 10000
 volumeA = 1.0
 volumeB = 1.0
+phase = Sig(.75)
+dt = Sig(.2)
 
-dt = Sig(.25)
-dt.ctrl(None, "Tempo")
+phase.ctrl([SLMap(0., 1., 'lin', "value", phase.value)], "Phase") # NOTE: the 'name' must be the name of attribute
+dt.ctrl([SLMap(0.1, 0.5, 'lin', "value", dt.value)], "Tempo") # NOTE: the 'name' must be the name of attribute
 
-# c = [SLMap(.0, 5., 'lin', "Volume", 1.0)]
-# volumeB.ctrl(c, "Beat Volume")
 
+# High drone
 oscs = [
-    Pad(note=root     , bw=40, damp=dampFactor*0.9, cutoff=cutoff, reverb=.9, mul= volumeA * 0.800), #  0.546   0.000
-    Pad(note=root+12  , bw=40, damp=dampFactor*0.8, cutoff=cutoff, reverb=.9, mul= volumeA * 0.800), #  0.542   0.000
-    Pad(note=root+24  , bw=40, damp=dampFactor*0.8, cutoff=cutoff, reverb=.9, mul= volumeA * 0.326), #  0.204   0.000
-    Pad(note=root+24+7, bw=40, damp=dampFactor*0.8, cutoff=cutoff, reverb=.9, mul= volumeA * 0.123), #  0.077   0.000
-    Pad(note=root+36  , bw=50, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, mul= volumeA * 0.179), #  0.112   0.065
-    Pad(note=root+36+7, bw=50, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, mul= volumeA * 0.056), #  0.035   0.023
-    Pad(note=root+48  , bw=50, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, mul= volumeA * 0.080), #  0.050   0.073
-    Pad(note=root+48+7, bw=50, damp=dampFactor*0.5, cutoff=cutoff, reverb=.9, mul= volumeA * 0.030), #  0.019   0.042
-    Pad(note=root+60  , bw=50, damp=dampFactor*0.4, cutoff=cutoff, reverb=.9, mul= volumeA * 0.067), #  0.042   0.027
-    Pad(note=root+60+7, bw=50, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, mul= volumeA * 0.019), #  0.012   0.023
-    Pad(note=root+72  , bw=50, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, mul= volumeA * 0.040), #  0.042   0.027
-    Pad(note=root+72+7, bw=50, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, mul= volumeA * 0.010), #  0.012   0.023
-    # ToneBeatSimple(note=root, dur=64*dt, decay=12., sustain=.05,    reverb=.3, mul= volumeB * 0.110, tone=[.2, 1., 0.8, 0.6, 0.2, .05]), #  0.069   0.000
-    # ToneBeatSimple(note=root, dur=dt, decay=.2, sustain=.0,    reverb=.3, mul= volumeB * 0.110, tone=[.8, 1., 0.8, 0.6, 0.2, .05]), #  0.069   0.000
-
-    ToneBeatSimple(note=root   , dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.040, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
-    ToneBeatSimple(note=root+12, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.040, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
-    ToneBeatSimple(note=root+24, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.025, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
-    ToneBeatSimple(note=root+36, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.008, tone=[1., .4, .2, .1, .05]), #  0.069   0.000
-
-    # ToneBeatSimple(note=root+36+7, dur=dt, decay=.2, sustain=.0,    reverb=.8, mul= volume * 0.010, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
-    # ToneBeatSimple(note=root+36, dur=.5*dt, decay=.1, sustain=.2,   reverb=.3, mul= volume * 0.070, tone=[1, 0.3, 0.1, 0.02, 0.005]), #  0.123   0.000
-    # # Recorded(soundPath="./data/Clean Combo#03.wav", cutoff=cutoff, mul=volume*4.0, reverb=0.),
-    # # Recorded(soundPath="./data/Clean Combo#01.wav", cutoff=cutoff, mul=volume*2.0, reverb=0.5),
-    # Recorded(soundPath="./data/Clean Combo#05.wav", cutoff=cutoff, mul=volume*3.0, reverb=0.5),
-    # RecordedGuit(soundPath="./data/AmbientE Loops.Guit.1.wav", cutoff=cutoff, mul=volume*0.06, lfo=.003, reverb=0.5),
-    # RecordedGuit(soundPath="./data/AmbientE Loops.Guit.3.wav", cutoff=cutoff, mul=volume*0.06, lfo=.003, reverb=0.5),
-    # RecordedGuit(soundPath="./data/AmbientE Loops.Guit.4.wav", cutoff=cutoff, mul=volume*0.06, lfo=.003, reverb=0.5),
+    Pad(note=root+36  , bw=70, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.179), #  0.112   0.065
+    Pad(note=root+36+7, bw=70, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.056), #  0.035   0.023
+    Pad(note=root+48  , bw=70, damp=dampFactor*0.6, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.080), #  0.050   0.073
+    Pad(note=root+48+7, bw=70, damp=dampFactor*0.5, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.030), #  0.019   0.042
+    Pad(note=root+60  , bw=70, damp=dampFactor*0.4, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.067), #  0.042   0.027
+    Pad(note=root+60+7, bw=70, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.019), #  0.012   0.023
+    Pad(note=root+72  , bw=70, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.040), #  0.042   0.027
+    Pad(note=root+72+7, bw=70, damp=dampFactor*0.3, cutoff=cutoff, reverb=.9, phase=phase, mul= volumeA * 0.010), #  0.012   0.023
+    
+    ToneBeatSimple(note=root+24, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.020, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
+    ToneBeatSimple(note=root+36, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.010, tone=[1., .4, .2, .1, .05]), #  0.069   0.000
+    ToneBeatSimple(note=root+48, dur=dt, decay=.12, sustain=.0,    reverb=.9, mul= volumeB * 0.006, tone=[1., .4, .2, .1, .05]), #  0.069   0.000
 ]
 
-def printAmps():
-    [print("{:.3f}".format(osc.amp.value.value)) for osc in oscs if hasattr(osc.amp.value, 'value')]
+# # Old stuff
+# oscs = [
+#     # ToneBeatSimple(note=root, dur=64*dt, decay=12., sustain=.05,    reverb=.3, mul= volumeB * 0.110, tone=[.2, 1., 0.8, 0.6, 0.2, .05]), #  0.069   0.000
+#     # ToneBeatSimple(note=root, dur=dt, decay=.2, sustain=.0,    reverb=.3, mul= volumeB * 0.110, tone=[.8, 1., 0.8, 0.6, 0.2, .05]), #  0.069   0.000
+#     # ToneBeatSimple(note=root+36+7, dur=dt, decay=.2, sustain=.0,    reverb=.8, mul= volume * 0.010, tone=[1., .8, .4, .2, .05]), #  0.069   0.000
+#     # ToneBeatSimple(note=root+36, dur=.5*dt, decay=.1, sustain=.2,   reverb=.3, mul= volume * 0.070, tone=[1, 0.3, 0.1, 0.02, 0.005]), #  0.123   0.000
+
+#     # # Recorded(soundPath="./data/Clean Combo#03.wav", cutoff=cutoff, mul=volume*4.0, reverb=0.),
+#     # # Recorded(soundPath="./data/Clean Combo#01.wav", cutoff=cutoff, mul=volume*2.0, reverb=0.5),
+#     # Recorded(soundPath="./data/Clean Combo#05.wav", cutoff=cutoff, mul=volume*3.0, reverb=0.5),
+#     # RecordedGuit(soundPath="./data/AmbientE Loops.Guit.1.wav", cutoff=cutoff, mul=volume*0.06, lfo=.003, reverb=0.5),
+#     # RecordedGuit(soundPath="./data/AmbientE Loops.Guit.3.wav", cutoff=cutoff, mul=volume*0.06, lfo=.003, reverb=0.5),
+#     # RecordedGuit(soundPath="./data/AmbientE Loops.Guit.4.wav", cutoff=cutoff, mul=volume*0.06, lfo=.003, reverb=0.5),
+# ]
+
+coef = Sig(0)
+coef.ctrl()
+
+# mixvol = [
+#     Sin(coef * Sig(2 * pi * 2))
+# ]
+
+def printInfos():
+    [print("{:.3f}".format(osc.amp.value.value)) if hasattr(osc.amp.value, 'value') else print("{:.3f}".format(osc.amp.value)) for osc in oscs]
+    # print("{:.3f}".format(coef.value.value)) if hasattr(coef.value, 'value') else print("{:.3f}".format(coef.value))
+    # [print("{:.3f}".format(v.value)) for v in mixvol]
     print("-")
 
-pool = Pattern(printAmps, 5.)
+pool = Pattern(printInfos, .5)
 pool.play()
 
 s.gui(locals())
