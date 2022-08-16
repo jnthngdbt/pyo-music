f729373abcc7f3b97ee3ea57977aea5904cd46ed
Jonathan Godbout <jon.godbout@gmail.com>
2022-08-15 23:06:26 -0400

    Got rid of unison detune beating

--- python/20220813.pyo.unison.detune.effects.py
+++ python/20220813.pyo.unison.detune.effects.py
@@ -1,13 +1,15 @@
 from pyo import *
+import numpy as np
 
 s = Server().boot()
 s.start()
 
-f = 200
-d = Sig(1) # detune
+f = 100
+d = Sig(.35) # detune
 d.ctrl([SLMap(0., 10., 'lin', "value", d.value)], "Detune") # NOTE: the 'name' must be the name of attribute
 
-nbVoicesEachSide = 3
+nbVoicesEachSide = 8
+nbVoices = nbVoicesEachSide * 2
 
 def getUnisonFreqs(freq):
   return [freq+i*d for i in range(-nbVoicesEachSide, nbVoicesEachSide)]
@@ -16,11 +18,14 @@ t = SawTable()
 
 md = hzToMidi(f)
 
+#
+# AH, to eliminate interference beating, each voice must have a random phase.
+#
 x = [
-  Osc(t, freq=getUnisonFreqs(midiToHz(md)), mul=.02, phase=0).out(),
-  Osc(t, freq=getUnisonFreqs(midiToHz(md+7)), mul=.015, phase=0).out(),
-  Osc(t, freq=getUnisonFreqs(midiToHz(md+12)), mul=.01, phase=0).out(),
-  Osc(t, freq=getUnisonFreqs(midiToHz(md+16)), mul=.008, phase=0).out(),
+  Osc(t, freq=getUnisonFreqs(midiToHz(md)), mul=.02, phase=np.random.random(nbVoices).tolist()).out(),
+  Osc(t, freq=getUnisonFreqs(midiToHz(md+7)), mul=.015, phase=np.random.random(nbVoices).tolist()).out(),
+  Osc(t, freq=getUnisonFreqs(midiToHz(md+12)), mul=.01, phase=np.random.random(nbVoices).tolist()).out(),
+  # Osc(t, freq=getUnisonFreqs(midiToHz(md+16)), mul=.008, phase=np.random.random(nbVoices).tolist()).out(),
 ]
 
 
