c98a331047557608bc3440263a881282df608261
Jonathan Godbout <jon.godbout@gmail.com>
2022-08-19 16:00:18 -0400

    Added SuperSaw for comparison

--- python/20220813.pyo.unison.detune.effects.py
+++ python/20220813.pyo.unison.detune.effects.py
@@ -5,6 +5,8 @@ s = Server().boot()
 s.start()
 
 f = 100
+
+# ---------------------------------------------
 d = Sig(.35) # detune
 d.ctrl([SLMap(0., 10., 'lin', "value", d.value)], "Detune") # NOTE: the 'name' must be the name of attribute
 
@@ -31,8 +33,23 @@ x = [
 p = []
 p.append(Mix(x, 2))
 # pipeline.append(Freeverb(pipeline[-1], size=[.99,.95], damp=.5, bal=0.5)), pipeline[-1].ctrl()
+p.append(WGVerb(p[-1])), p[-1].ctrl()
 p.append(Delay(p[-1], delay=[.15, .2], feedback=.0)), p[-1].ctrl()
 p.append(MoogLP(p[-1], freq=20000, res=0.0)), p[-1].ctrl()
 p[-1].out()
+# ---------------------------------------------
+
+# ---------------------------------------------
+# Ah, did not see that!
+
+# p = []
+# p.append(SuperSaw([f,f])), p[-1].ctrl()
+# # pipeline.append(Freeverb(pipeline[-1], size=[.99,.95], damp=.5, bal=0.5)), pipeline[-1].ctrl()
+# p.append(WGVerb(p[-1])), p[-1].ctrl()
+# p.append(Delay(p[-1], delay=[.15, .2], feedback=.0)), p[-1].ctrl()
+# p.append(MoogLP(p[-1], freq=20000, res=0.0)), p[-1].ctrl()
+# p[-1].out()
+
+# ---------------------------------------------
 
 s.gui(locals())
