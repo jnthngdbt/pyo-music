Fixed overflow and mix

python/20220821.fft.filter.realtime.py
@@ -17,7 +17,7 @@ s.start()
 # minFftPeriod = minFreqNbCycles * minFreqPeriod # 2*.05=.1 > 4410
 # minFftN = fs * minFftPeriod
 
-N = 4096*8
+N = 2**14 # 2**14=16k
 Nh = int(N/2)
 
 a = Noise(.25).mix(2)
@@ -26,21 +26,21 @@ fin = FFT(a, size=N, overlaps=4, wintype=2)
 def getSigVal(sig):
   return sig.value.value if hasattr(sig.value, 'value') else sig.value
 
+p0 = Sig(100)
+p0.ctrl([SLMap(5., 500., 'lin', "value", p0.value)], "P0")
+
 bw = Sig(10)
 bw.ctrl([SLMap(5., 50., 'lin', "value", bw.value)], "BW")
-
-p0 = Sig(200)
-p0.ctrl([SLMap(50., 500., 'lin', "value", p0.value)], "P0")
 
 bws = Sig(.7)
 bws.ctrl([SLMap(.1, 2.0, 'lin', "value", bws.value)], "BWS")
 
-dmp = Sig(1.)
+dmp = Sig(.7)
 dmp.ctrl([SLMap(.1, 10., 'lin', "value", dmp.value)], "DMP")
 
 def createSpectrum():
   s = np.zeros(Nh)
-  for i in range(32):
+  for i in range(16):
     Nw = int((i * getSigVal(bws) + 1) * getSigVal(bw))
     if Nw < 3: Nw = 3 # minimum length
     if Nw % 2 == 0: Nw += 1 # make sure odd
@@ -48,7 +48,11 @@ def createSpectrum():
     damping = np.exp(-i*getSigVal(dmp))
     w = 10 * damping * signal.windows.hann(Nw)
     p = int(getSigVal(p0) * (i + 1))
-    s[p-Nwh : p + Nwh + 1] += w # slicing: [a,b[
+
+    a = max(0, p-Nwh)
+    b = min(Nh-1, p + Nwh + 1)
+    Nab = b-a
+    s[a:b] += w[0:Nab] # slicing: [a,b[
   return s
 
 v = createSpectrum()
@@ -63,8 +67,8 @@ im = fin["imag"] * amp
 fout = IFFT(re, im, size=N, overlaps=4, wintype=2).mix(2)
 
 p = []
-p.append(Mix(fout, 2, mul=.2))
-p.append(Freeverb(p[-1], size=[.5,.6]))
+p.append(fout)
+# p.append(Freeverb(p[-1], size=[.5,.6])), p[-1].ctrl()
 p[-1].out()
 
 Spectrum(fout, size=N)