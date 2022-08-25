cbbbc10cb3527d07bcc91c2b43183e9f32dbb90f
Jonathan Godbout <jon.godbout@gmail.com>
2022-08-24 23:02:02 -0400

    Using BrownNoise, separate band and peak

--- python/20220111.pyo.peak.band.py
+++ python/20220111.pyo.peak.band.py
@@ -7,17 +7,21 @@ s.start()
 # https://mynoise.net/NoiseMachines/peakNoiseGenerator.php?l=03030303039903030303
 # https://mynoise.net/NoiseMachines/whiteNoiseGenerator.php?l=02020202029902020202
 
-# Best peak noise 1 (almost like myNoise): 2 pink noise streams, biquadx 2 stages 10q
+# Best peak noise 1 (almost like myNoise): 2 brown noise streams, biquadx 2 stages 10q
 
-freq = midiToHz(85) # C# 85, like slider 6/10 on myNoise
+freq = midiToHz(73) # C# 85, like slider 6/10 on myNoise
 
-noise1 = PinkNoise()
-noise2 = PinkNoise()
-band = Biquadx([noise1, noise2], freq=freq, q=10, type=2, stages=2, mul=.5)
+noise1 = BrownNoise()
+noise2 = BrownNoise()
+band = Biquadx([noise1, noise2], freq=freq, q=2.3, type=2, stages=3, mul=.3)
+peak = Biquadx([noise1, noise2], freq=freq, q=26,  type=2, stages=2, mul=2.)
 
 band.ctrl()
-band.out()
+peak.ctrl()
 
-Spectrum(band)
+p = band + peak
+p.out()
+
+Spectrum(p)
 
 s.gui(locals())
