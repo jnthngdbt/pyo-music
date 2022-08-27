b2ee5b333bbd2d2a38d239c8d78fb990d95b0c46
Jonathan Godbout <jon.godbout@gmail.com>
2022-08-26 22:35:55 -0400

    Added BigBass

--- python/20220824.pyo.peak.ambient.py
+++ python/20220824.pyo.peak.ambient.py
@@ -17,7 +17,7 @@ def randRange(a,b):
 # https://mynoise.net/NoiseMachines/peakNoiseGenerator.php?l=00000000304030000000&a=2&am=5&d=-3 (A#)
 
 class Base:
-    def __init__(self, lfo=0.015, mul=1.):
+    def __init__(self, lfo=0.03, mul=1.):
         self.amp = Sig(mul)
         self.lfoMulFreq = Sine(freq=lfo*randRange(.2, .5), phase=randRange(0, 1)).range(lfo*.5, lfo*1.5) # LFO of amplitude LFO frequency
         self.lfoMul = self.amp * Sine(freq=self.lfoMulFreq, phase=randRange(0, 1)).range(0, 1) # LFO for amplitude modulation
@@ -38,6 +38,19 @@ class Peak (Base):
         self.peak = Biquadx([self.noise1, self.noise2], freq=self.freq, q=self.qp,  type=2, stages=2, mul=mulp*self.lfoMul)
         self.out = self.band + self.peak
 
+# Stay below midi(30) or 60Hz for some growl.
+class BigBass (Base):
+    def __init__(self, note=25, **kwargs):
+        super().__init__(**kwargs)
+
+        self.p = []
+        self.p.append(HarmTable([.5, 1, .7, .2])) # sub 0, main, fifth, octave
+        self.p.append(Osc(self.p[-1], midiToHz(note)))
+        self.p.append(Chorus(self.p[-1], [1, 1.2], feedback=0)) # adds some movement
+        self.p[-1].mul = self.lfoMul
+        
+        self.out = self.p[-1]
+
 # -----------------------------------------------------------------------------------------------
 
 root = 73
@@ -53,35 +66,38 @@ oscs = [
     # Peak(note=root-48+ 0, mul=m1, mulp=mulp, lfo=0.005),
     # Peak(note=root-36+ 0, mul=m1, mulp=mulp, lfo=0.005),
     #
-    # Peak(note=root-24+ 0, mul=m2, mulp=mulp),
+    BigBass(note=root-48+ 0, mul=.2*m0, lfo=0.01), # better if around midi(25)
+    BigBass(note=root-36+ 0, mul=.1*m1, lfo=0.01), # better if around midi(37)
+    #
+    Peak(note=root-24+ 0, mul=m2, mulp=mulp),
     Peak(note=root-12+ 0, mul=m1, mulp=mulp),
     Peak(note=root   + 0, mul=m0, mulp=mulp),
     Peak(note=root+12+ 0, mul=m1, mulp=mulp),
     Peak(note=root+24+ 0, mul=m2, mulp=mulp),
     #
-    # Peak(note=root-24+ 4, mul=m2, mulp=mulp),
-    # Peak(note=root-12+ 4, mul=m1, mulp=mulp),
-    # Peak(note=root   + 4, mul=m0, mulp=mulp),
-    # Peak(note=root+12+ 4, mul=m1, mulp=mulp),
-    # Peak(note=root+24+ 4, mul=m2, mulp=mulp),
-    # #
-    # Peak(note=root-24+ 5, mul=m2, mulp=mulp),
-    # Peak(note=root-12+ 5, mul=m1, mulp=mulp),
-    # Peak(note=root   + 5, mul=m0, mulp=mulp),
-    # Peak(note=root+12+ 5, mul=m1, mulp=mulp),
-    # Peak(note=root+24+ 5, mul=m2, mulp=mulp),
-    # #
-    # Peak(note=root-24+ 7, mul=m2, mulp=mulp),
+    Peak(note=root-24+ 4, mul=m2, mulp=mulp),
+    Peak(note=root-12+ 4, mul=m1, mulp=mulp),
+    Peak(note=root   + 4, mul=m0, mulp=mulp),
+    Peak(note=root+12+ 4, mul=m1, mulp=mulp),
+    Peak(note=root+24+ 4, mul=m2, mulp=mulp),
+    #
+    Peak(note=root-24+ 5, mul=m2, mulp=mulp),
+    Peak(note=root-12+ 5, mul=m1, mulp=mulp),
+    Peak(note=root   + 5, mul=m0, mulp=mulp),
+    Peak(note=root+12+ 5, mul=m1, mulp=mulp),
+    Peak(note=root+24+ 5, mul=m2, mulp=mulp),
+    #
+    Peak(note=root-24+ 7, mul=m2, mulp=mulp),
     Peak(note=root-12+ 7, mul=m1, mulp=mulp),
     Peak(note=root   + 7, mul=m0, mulp=mulp),
     Peak(note=root+12+ 7, mul=m1, mulp=mulp),
-    ##Peak(note=root+24+ 7, mul=m2, mulp=mulp),
-    # #
-    # Peak(note=root-24+ 9, mul=m2, mulp=mulp),
-    # Peak(note=root-12+ 9, mul=m1, mulp=mulp),
-    # Peak(note=root   + 9, mul=m0, mulp=mulp),
-    # Peak(note=root+12+ 9, mul=m1, mulp=mulp),
-    # ##Peak(note=root+24+ 9, mul=m2, mulp=mulp),
+    #Peak(note=root+24+ 7, mul=m2, mulp=mulp),
+    #
+    Peak(note=root-24+ 9, mul=m2, mulp=mulp),
+    Peak(note=root-12+ 9, mul=m1, mulp=mulp),
+    Peak(note=root   + 9, mul=m0, mulp=mulp),
+    Peak(note=root+12+ 9, mul=m1, mulp=mulp),
+    ##Peak(note=root+24+ 9, mul=m2, mulp=mulp),
 ]
 
 p = []
