203bfbfed0ecf01d8620b0aa0a16df5168a4e818
Jonathan Godbout <jon.godbout@gmail.com>
2022-08-30 22:15:51 -0400

    New files analyzed

--- python/20220824.pyo.file.spectrum.py
+++ python/20220824.pyo.file.spectrum.py
@@ -9,7 +9,8 @@ s.start()
 # t = SndTable("C:\\Users\\jgodbout\\Documents\\git\\sandbox\\python\\songs\\slider.spectrum.sample.wav")
 # t = SndTable("C:\\Users\\jgodbout\\OneDrive\\Music\\myNoise\\WhitePeak\\3a.ogg") # C# 250 Hz
 # t = SndTable("C:\\Users\\jgodbout\\OneDrive\\Music\\myNoise\\WhitePeak\\4a.ogg") # C# 500 Hz
-t = SndTable("C:\\Users\\jgodbout\\OneDrive\\Music\\myNoise\\Northern\\5a.ogg") # peaks: 141, 191
+# t = SndTable("C:\\Users\\jgodbout\\OneDrive\\Music\\myNoise\\Northern\\5a.ogg") # peaks: 141, 191
+t = SndTable("C:\\Users\\jgodbout\\OneDrive\\Music\\myNoise\\OakSongRiikka\\9a.ogg") # peaks: 240(C), 287(D), 381(G), 430(A)
 
 f = s.getSamplingRate() / t.size
 x = Osc(t, freq=f).out()
