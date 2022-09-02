a198ad3312ef5528324fa85bc066fdf02981a1bf
Jonathan Godbout <jon.godbout@gmail.com>
2022-09-01 21:36:30 -0400

    Changed envelope

--- python/20220512.pyo.mission.ten.py
+++ python/20220512.pyo.mission.ten.py
@@ -5,8 +5,10 @@ from pyoband import band
 s = Server().boot()
 
 root = 25 # 30
-dur = .22 #.182
+dur = .182 #.182
 gain = 1.2
+
+env = [(0,0.0000),(35,0.7576),(178,0.5636),(8192,0.8121)]
 
 M = band.Maestro(time=dur, nbSectionsToggle=0, tracks=[
         band.BassBeat(name="Bass Beat", mul = gain*0.02, root = root, adsr=[.02, .02, .4, .02], dur=0.18, tone=[1, 1, .6, .2, .1],
@@ -14,19 +16,19 @@ M = band.Maestro(time=dur, nbSectionsToggle=0, tracks=[
             beat = [ 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0 ],
             note = [ 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]
         ),
-        band.PadLowGrowl(name="Low Mid", mul = gain*0.2, root = root, dur=16*dur, reverb=.1, samplingRate=s.getSamplingRate(),
+        band.PadLowGrowl(name="Low Mid", mul = gain*0.2, root = root, env=env, dur=16*dur, reverb=.1, samplingRate=s.getSamplingRate(),
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
             note = [ 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]
         ),
-        band.PadLowString(name="Low Mid", mul = gain*0.045, root = root+12, dur=16*dur, reverb=.1, samplingRate=s.getSamplingRate(),
+        band.PadLowString(name="Low Mid", mul = gain*0.045, root = root+12, env=env, dur=16*dur, reverb=.1, samplingRate=s.getSamplingRate(),
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
             note = [i + 0 for i in
                    [ 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]
                    ]
         ),
-        band.PadHighStringSoft(name="High", mul = gain*0.01, root = root+36, dur=16*dur, reverb=.1, samplingRate=s.getSamplingRate(), env=[(0,0.0000),(71,0.0848),(4972,0.2061),(8102,0.2121),(8192,0.0000)],
+        band.PadHighStringSoft(name="High", mul = gain*0.01, root = root+36, env=env, dur=16*dur, reverb=.1, samplingRate=s.getSamplingRate(),
             #        |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x  |  x  x  x  X  x  x  x  X  x  x  x  X  x  x  x
             beat = [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
             note = [12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11]
