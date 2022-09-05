0bb03703ff974245b23bfbe2be5b9cd5e76b3344
Jonathan Godbout <jon.godbout@gmail.com>
2022-09-04 21:08:51 -0400

    Poor man Hans Zimmer bass punch kinda

--- python/20220902.pyo.massive.bass.punch.py
+++ python/20220902.pyo.massive.bass.punch.py
@@ -4,27 +4,52 @@ from pyo import *
 s = Server().boot()
 s.start()
 
-# Stay below midi(30) or 60Hz for some growl.
+class BassString:
+  def __init__(self, f) -> None:
+    padSize = 262144 * 2
+    padFreq = 440
+    padRatio = s.getSamplingRate() / padSize
+    freqRatio = padRatio * f / padFreq
+    self.osc = Osc(PadSynthTable(basefreq=padFreq, size=padSize, spread=1, bw=50, bwscl=1.0, damp=.9, nharms=64), freq=freqRatio)
+
+  def out(self): 
+    self.osc.out()
+
+class BassTone:
+  def __init__(self, f) -> None:
+    self.table = HarmTable([.5, 1, .8, .5])
+    self.osc = Osc(self.table, f)
+
+  def out(self): 
+    self.osc.out()
+
 class BigBass:
-  def __init__(self, beat, f=midiToHz(25), mul=1) -> None:
-    # self.env = Adsr(attack=.01, decay=0.01, sustain=0.8, release=4, dur=5)
-    self.env = TrigExpseg(beat, [(0,0), (.05,1), (.1, .8), (4, 0)])
+  def __init__(self, instrument, mul=1, dur=1) -> None:
+    self.env = Adsr(attack=.01, decay=.3, sustain=.8, release=.15, dur=dur)
+    self.env.setExp(1)
+    self.env.ctrl()
 
     self.p = []
-    self.p.append(HarmTable([.5, 1, .7, .2])) # sub 0, main, fifth, octave
-    self.p.append(Osc(self.p[-1], f))
-    self.p[-1].ctrl()
-    self.p.append(Chorus(self.p[-1], [1, 1.2], feedback=0)) # adds some movement
+    self.p.append(instrument)
+    self.p.append(Freeverb(self.p[-1].osc, size=[.7,.8], bal=0.5)) # adds some movement
     self.p[-1].mul = mul * self.env
-    self.p[-1].ctrl()
     self.p[-1].out()
 
+  def play(self):
+    self.env.play()
 
+dur = 2.5
+interval = 4
 
-trig = Beat(time=.125, w1=80, w2=0, w3=0).play()
-B = BigBass(trig, midiToHz(25), mul=.2) # better if around midi(25)
-B = BigBass(trig, midiToHz(37), mul=.1) # better if around midi(37)
+B = [
+  BigBass(instrument=BassTone(midiToHz(25)), mul=.3, dur=dur),
+  BigBass(instrument=BassString(midiToHz(25)), mul=.5, dur=dur),
+  BigBass(instrument=BassString(midiToHz(37)), mul=.3, dur=dur),
+]
 
-Spectrum(B.p[-1], size=8192)
+def playEnv():
+  [b.play() for b in B]
+
+p = Pattern(playEnv, time=interval).play()
 
 s.gui(locals())
