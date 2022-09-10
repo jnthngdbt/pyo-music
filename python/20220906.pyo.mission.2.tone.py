5bd5832ce0e5b832951f7ad7f0d07141dafd1a86
Jonathan Godbout <jon.godbout@gmail.com>
2022-09-09 20:56:56 -0400

    Add wait argument to InstrumentPlayer

--- python/20220906.pyo.mission.2.tone.py
+++ python/20220906.pyo.mission.2.tone.py
@@ -18,13 +18,13 @@ class Horn:
     self.p[-1].mul = self.env * mul
     self.p[-1].out()
 
+    if hasattr(self.env, 'ctrl'): self.env.ctrl()
+    if hasattr(self.envf, 'graph'): self.envf.graph()
+
   def playNote(self, note):
-    try:
-      self.envf.play()
-      self.env.play()
-      self.osc.freq = self.envf * midiToHz(note)
-    except:
-      print("To use beats, must define envelopes env and envf")
+    if hasattr(self.envf, 'play'): self.envf.play()
+    if hasattr(self.env, 'play'): self.env.play()
+    self.osc.freq = self.envf * midiToHz(note)
 
   def getOsc(self):
     return self.p[-1]
@@ -60,22 +60,31 @@ class NoiseBaseline:
     return self.p[-1]
 
 class InstrumentPlayer:
-  def __init__(self, instrument, root=49, note=[0], beat=[1], time=0.25, interval=0):
+  def __init__(self, instrument, root=49, note=[0], beat=[1], time=0.25, interval=0, wait=0):
     self.instrument = instrument
     self.beat = beat
     self.root = root
     self.note = note
     self.time = time
     self.interval = interval
+    self.wait = wait
 
     self.size = max([len(beat), len(note)])
     self.tickIdx = 0
     self.isInterval = False
+    self.isWaiting = wait > 0
     
     self.clock = Pattern(self.tick, time=self.time).play()
 
   def tick(self):
-    if self.isInterval:
+    if self.isWaiting:
+      if self.tickIdx < self.wait:
+        self.tickIdx = self.tickIdx + 1
+        return # wait
+      else:
+        self.tickIdx = 0
+        self.isWaiting = False
+    elif self.isInterval:
       if self.tickIdx < self.interval:
         self.tickIdx = self.tickIdx + 1
         return # wait
@@ -98,12 +107,20 @@ class InstrumentPlayer:
 
 x = [
   BassString(32, mul=1),
-  Horn([56,63], mul=.02, tone=[1., .5, .25]),
+  Horn([56], mul=.02, tone=[1., .5, .25]),
   InstrumentPlayer(
-    Horn(62, mul=.1, tone=[1., .5, .25], env=Adsr(.02, 18, 0, 0), envf=Linseg([(0,.1),(.01,1)])),
-    beat=[ 1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0, 1,0,1],
-    note=[-5,0,2,0,0,0,0,0,4,5,4,0,0,0,0,0,0,0,0,0,0,0,0,2,0,-2,0,0],
-    root=56, time=.2, interval=60
+    Horn(63, mul=.04, tone=[1., .5, .25], env=Adsr(.04, 0, 1, 0)),
+    beat=[1],
+    note=[0],
+    root=63, time=.2, wait=4, interval=40
+  ),
+  InstrumentPlayer(
+    Horn(56, mul=.1, tone=[1., .5, .25], env=Adsr(.03, 120, 0, 0), envf=Linseg([(0,0.4),(.01,1)])),
+    beat=[ 1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    note=[-5,0,2,0,0,0,0,0,4,5,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    # beat=[ 1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0, 1,0,1],
+    # note=[-5,0,2,0,0,0,0,0,4,5,4,0,0,0,0,0,0,0,0,0,0,0,0,2,0,-2,0,0],
+    root=56, time=.2, interval=60, wait=20
   ),
   NoiseBaseline(mul=.01),
 ]
